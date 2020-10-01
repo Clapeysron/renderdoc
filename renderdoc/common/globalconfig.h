b2b2152e22063bc64b2c729bedd9d843f191740f
Clapeysron <clapeysron@qq.com>
2020-10-01 13:03:00 +0800

    Modify apk lib&label name

--- renderdoc/common/globalconfig.h
+++ renderdoc/common/globalconfig.h
@@ -164,9 +164,9 @@ enum
   RenderDoc_ForwardPortStride = 10,
 };
 
-#define RENDERDOC_VULKAN_LAYER_NAME "VK_LAYER_RENDERDOC_Capture"
+#define RENDERDOC_VULKAN_LAYER_NAME "VK_LAYER_QSMRDC_Capture"
 
-#define RENDERDOC_ANDROID_LIBRARY "libVkLayer_GLES_RenderDoc.so"
+#define RENDERDOC_ANDROID_LIBRARY "libVkLayer_GLES_QSMRDC.so"
 
 // This MUST match the package name in the build process that generates per-architecture packages
 #define RENDERDOC_ANDROID_PACKAGE_BASE "org.qsm.rdc"
